f6409ee69745a7a7dacafaabaab34f351efdee4e
Marcin J <jerzakm@gmail.com>
2021-06-11 11:44:42 +0200

    implemented SMUI

--- src/lib/Menus/Header.svelte
+++ src/lib/Menus/Header.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import { logOut } from '$lib/auth';
+
+	import Button from '@smui/button/Button.svelte';
 </script>
 
 <header>
@@ -11,7 +13,7 @@
 			<li class:active={$page.path === '/'}><a sveltekit:prefetch href="/">Home</a></li>
 			<li class:active={$page.path === '/about'}><a sveltekit:prefetch href="/about">About</a></li>
 		</ul>
-		<button on:click={() => logOut()}>Logout</button>
+		<Button on:click={() => logOut()}>Log out</Button>
 	</nav>
 </header>
 
