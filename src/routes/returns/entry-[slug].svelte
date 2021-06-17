4322edefda2875ac9e6c04e18d8c71500885a26e
Marcin J <jerzakm@gmail.com>
2021-06-17 14:40:46 +0200

    returns list routes

---
+++ src/routes/returns/entry-[slug].svelte
@@ -0,0 +1,18 @@
+<script context="module">
+	/**
+	 * @type {import('@sveltejs/kit').Load}
+	 */
+	export async function load({ page, fetch, session, context }) {
+		return {
+			props: {
+				returnId: page.params.slug
+			}
+		};
+	}
+</script>
+
+<script>
+	export let returnId;
+</script>
+
+<h1>{returnId}</h1>
