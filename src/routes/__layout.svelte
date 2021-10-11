25b607c8e9537131f372974ae533536e179ac532
Marcin J <jerzakm@gmail.com>
2021-10-11 14:23:33 +0200

    moved styles

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,10 +3,6 @@
 	import TopBar from '$lib/Menus/TopBar.svelte';
 	import Button, { Label, Icon } from '@smui/button';
 	import Fab from '@smui/fab';
-	import IconButton from '@smui/icon-button';
-
-	import '../styles/tailwind-output.css';
-	import '../app.css';
 
 	import { goto } from '$app/navigation';
 	import { authStore, checkStoredLogin } from '$lib/auth';
@@ -42,6 +38,21 @@
 
 	setContext('notification', notification);
 </script>
+
+<svelte:head>
+	<!-- Fonts and CSS -->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700" />
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Mono" />
+
+	<!-- tailwind -->
+	<link rel="stylesheet" href="/styles/tailwind-output.css" />
+	<!-- smui -->
+	<link rel="stylesheet" href="/smui.css" />
+	<!-- app -->
+	<link rel="stylesheet" href="/styles/app.css" />
+	<!-- <link rel="stylesheet" href="/smui-dark.css" media="screen and (prefers-color-scheme: dark)" /> -->
+</svelte:head>
 
 <layout>
 	{#if user}
