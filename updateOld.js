e4005436429f5ca35a0340cfa553d4e70e0c3a49
Marcin J <jerzakm@gmail.com>
2021-10-11 11:46:41 +0200

    data migration script updated with product import

--- updateOld.js
+++ updateOld.js
@@ -10,13 +10,53 @@ async function start() {
 	const file = fs.readFileSync('oldData.json');
 	const data = JSON.parse(file);
 
-	for (let i = 0; i < data.length; i++) {		
-		upsertEntry(data[i]);
+	await deleteAll();
+
+	for (let i = 0; i < data.length; i++) {
+		await upsertEntry(data[i]);
+		// await updateProducts(data[i]);
 	}
 }
 
+async function deleteAll() {
+	await prisma.returnEvent.deleteMany({});
+	await prisma.returnImage.deleteMany({});
+	await prisma.returnLocation.deleteMany({});
+	await prisma.returnNote.deleteMany({});
+	await prisma.returnProduct.deleteMany({});
+	await prisma.returnReason.deleteMany({});
+	await prisma.returnSender.deleteMany({});
+	await prisma.returnEntry.deleteMany({});
+}
+
 async function upsertEntry(entry) {
+	console.log(`updating ${entry.returnId}`);
 	const date = `${dayjs(entry.timestamp).format('YYYY-MM-DDTHH:mm:ss')}.500Z`;
+
+	const foundProducts = [];
+
+	for (const productEntry of entry.productEntryList) {
+		if (!productEntry || !productEntry.product) {
+			return;
+		}
+
+		const foundProduct = await prisma.product.findFirst({
+			where: {
+				symbol: productEntry.product.symbol
+			}
+		});
+
+		if (foundProduct) {
+			foundProducts.push({
+				description: JSON.stringify(productEntry.productStateId),
+				group: foundProduct.group,
+				name: productEntry.product.name,
+				price: productEntry.product.value,
+				quantity: productEntry.quantity,
+				symbol: productEntry.product.symbol
+			});
+		}
+	}
 
 	const e = await prisma.returnEntry.upsert({
 		where: {
@@ -35,6 +75,9 @@ async function upsertEntry(entry) {
 					postCode: entry.sender.postCode,
 					street: entry.sender.street
 				}
+			},
+			products: {
+				create: foundProducts
 			}
 		},
 		update: {
@@ -49,11 +92,50 @@ async function upsertEntry(entry) {
 					postCode: entry.sender.postCode,
 					street: entry.sender.street
 				}
+			},
+			products: {
+				create: foundProducts
 			}
 		}
 	});
 
 	console.log(e);
+}
+
+async function updateProducts(entry) {
+	console.log(`updating ${entry.returnId}`);
+
+	for (const productEntry of entry.productEntryList) {
+		if (!productEntry || !productEntry.product) {
+			return;
+		}
+
+		const foundProduct = await prisma.product.findFirst({
+			where: {
+				symbol: productEntry.product.symbol
+			}
+		});
+
+		if (foundProduct) {
+			const e = await prisma.returnEntry.update({
+				where: {
+					id: entry.returnId
+				},
+				data: {
+					products: {
+						create: {
+							description: JSON.stringify(productEntry.productStateId),
+							group: foundProduct.group,
+							name: productEntry.product.name,
+							price: productEntry.product.value,
+							quantity: productEntry.quantity,
+							symbol: productEntry.product.symbol
+						}
+					}
+				}
+			});
+		}
+	}
 }
 
 start();
