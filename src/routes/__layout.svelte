3d8c550e8f582281b6694601afe35bed3ee3dbbe
Marcin Jerzak <jerzakm@gmail.com>
2022-02-19 19:44:11 +0100

    simple logout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -7,6 +7,7 @@
 	import { goto } from '$app/navigation';
 	import { authStore, checkStoredLogin } from '$lib/core/auth';
 	import { onMount } from 'svelte';
+	import Topbar from '$lib/components/Nav/Topbar.svelte';
 
 	let user = null;
 	let mounted = false;
@@ -105,12 +106,7 @@
 		</div>
 	</div>
 	<main class="w-full flex flex-col flex-1 overflow-x-hidden" style="max-height: 100vh;">
-		<div class="p-2 max-h-12 h-12 border-b ">
-			<div class="flex justify-between">
-				<div class="text-sm flex items-center">topbar</div>
-				<div class="flex space-x-1 md:space-x-2">Xnav</div>
-			</div>
-		</div>
+		<Topbar {user} />
 		<div class="flex flex-col flex-1 overflow-y-auto">
 			<content class="flex-1 overflow-y-auto" style="max-height: 100vh;">
 				<slot />
