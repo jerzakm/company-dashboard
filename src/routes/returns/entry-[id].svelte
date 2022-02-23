71ceca9f3360d5103f11786d02f6b6e4aaf475c0
Marcin Jerzak <jerzakm@gmail.com>
2022-02-23 10:24:57 +0100

    route for editing/adding new return (entry-[id])

---
+++ src/routes/returns/entry-[id].svelte
@@ -0,0 +1,18 @@
+<script context="module">
+	/**
+	 * @type {import('@sveltejs/kit').Load}
+	 */
+	export async function load({ params }) {
+		return {
+			props: {
+				id: params.id
+			}
+		};
+	}
+</script>
+
+<script>
+	export let id;
+</script>
+
+<h1>Entry {id}</h1>
