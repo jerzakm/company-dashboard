8e6f99ce04392953a8a3245565befbb3d9feaff8
Marcin Jerzak <jerzakm@gmail.com>
2022-03-04 20:30:11 +0100

    simple image gallery modal

--- src/lib/components/core/Modal.svelte
+++ src/lib/components/core/Modal.svelte
@@ -12,14 +12,14 @@
 	<div
 		class="modal fixed inset-0 flex items-center justify-center overflow-y-auto overflow-x-hidden outline-none focus:outline-none"
 	>
-		<content class={$$props.class}>
+		<content>
 			<div class="flex justify-between py-2">
 				<h3>{title}</h3>
 				<Button on:click={() => (showModal = false)}>X</Button>
 			</div>
 			<Divider class="mb-2" />
 
-			<div class="my-4"><slot /></div>
+			<div class={`my-4 ${$$props.class}`}><slot /></div>
 		</content>
 	</div>
 	<div class="modal-bg fixed inset-0 bg-black opacity-25" />
--- src/routes/returns/entry-[id]/_components/ImageGallery.svelte
+++ src/routes/returns/entry-[id]/_components/ImageGallery.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Button from '$lib/components/core/Button.svelte';
+	import Modal from '$lib/components/core/Modal.svelte';
 
 	import { post } from '$lib/core/api';
 	import { formatToDateHour } from '$lib/util/time';
@@ -9,6 +10,9 @@
 	export let entry;
 
 	const dispatch = createEventDispatcher();
+
+	let showModal;
+	let modalImageIndex = 0;
 
 	let fileinput;
 
@@ -32,9 +36,55 @@
 </script>
 
 {#if entry}
+	<Modal title="Image gallery" bind:showModal class="flex flex-col items-center">
+		<img
+			src={`/api/images/${entry.images[modalImageIndex].id}.${entry.images[modalImageIndex].extension}`}
+			alt={`product or packaging image for return ${entry.id} - ${entry.images[modalImageIndex].description}`}
+			class="max-w-full "
+		/>
+
+		<close class="fixed top-0 right-4 p-4">
+			<Button size="large" on:click={() => (showModal = false)}>X</Button>
+		</close>
+
+		<controls class="fixed bottom-0 z-50 m-auto flex flex-col items-center gap-2">
+			<div class="flex flex-col items-center">
+				<span class="mt-1 px-2 text-xs "
+					>Added by <b>{entry.images[modalImageIndex].user.name}</b> on {formatToDateHour(
+						entry.images[modalImageIndex].created_at
+					)}</span
+				>
+				<span>{modalImageIndex + 1}/{entry.images.length}</span>
+			</div>
+			<div class="flex gap-4">
+				<Button
+					on:click={() => {
+						modalImageIndex > 0
+							? (modalImageIndex = modalImageIndex - 1)
+							: (modalImageIndex = entry.images.length - 1);
+						console.log(modalImageIndex);
+					}}>&lt Previous</Button
+				>
+				<Button
+					on:click={() => {
+						modalImageIndex < entry.images.length - 1
+							? (modalImageIndex = modalImageIndex + 1)
+							: (modalImageIndex = 0);
+						console.log(modalImageIndex);
+					}}>Next &gt</Button
+				>
+			</div>
+		</controls>
+	</Modal>
 	<div class="masonry">
-		{#each entry.images as image}
-			<figure class="flex flex-col items-center">
+		{#each entry.images as image, i}
+			<figure
+				class="flex flex-col items-center"
+				on:click={() => {
+					showModal = true;
+					modalImageIndex = i;
+				}}
+			>
 				<img
 					src={`/api/images/${image.id}.${image.extension}`}
 					alt={`product or packaging image for return ${entry.id} - ${image.description}`}
@@ -91,5 +141,8 @@
 	}
 	figure > img {
 		max-height: 40vh;
+	}
+	controls {
+		background-color: var(--background-color);
 	}
 </style>
