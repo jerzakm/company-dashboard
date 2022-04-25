2e24b287530e744ff7bb364f7eed38e349bf07b1
Marcin Jerzak <jerzakm@gmail.com>
2022-04-25 14:02:46 +0200

    product string search working

--- src/lib/returnLogic/returnListFilters.ts
+++ src/lib/returnLogic/returnListFilters.ts
@@ -1,13 +1,12 @@
 import * as removeAccents from 'remove-accents';
 
 export const processFilterQuery = (list, query) => {
-	console.log('process query');
-	console.log(list[10]);
-	console.log(query);
+	console.log(list[100]);
+	console.log('query', query);
 
 	//sender query
 	const filteredList = list.filter((entry) => {
-		return senderQuery(entry.sender, query.sender);
+		return senderQuery(entry.sender, query.sender) && productQuery(entry.products, query.product);
 	});
 
 	return filteredList;
@@ -23,4 +22,16 @@ const senderQuery = (sender, senderQuery) => {
 	const query = removeAccents.remove(senderQuery).toLowerCase().normalize().replace(/\W+/g, ' ');
 
 	return senderString.includes(query);
+};
+
+const productQuery = (products, productQuery) => {
+	// both sender and query strings have locale accents removed (like ą,ę,ź etc), are normalized (NFC), converted to lowercase and non alpha-numeric characters removed
+	const productString = removeAccents
+		.remove(JSON.stringify(products))
+		.toLowerCase()
+		.normalize()
+		.replace(/\W+/g, ' ');
+	const query = removeAccents.remove(productQuery).toLowerCase().normalize().replace(/\W+/g, ' ');
+
+	return productString.includes(query);
 };
--- src/routes/returns/list/_components/ReturnListFilters.svelte
+++ src/routes/returns/list/_components/ReturnListFilters.svelte
@@ -1,17 +1,31 @@
 <script>
 	import Input from '$lib/components/core/Input.svelte';
+	import { _ } from 'svelte-i18n';
 	import { createEventDispatcher } from 'svelte';
 
 	const dispatch = createEventDispatcher();
 
 	let query = {
-		sender: ''
+		sender: '',
+		product: ''
 	};
 </script>
 
-<Input
-	bind:value={query.sender}
-	on:input={() => {
-		dispatch('filterQuery', query);
-	}}
-/>
+<div class="px-2 py-4">
+	<div class="grid grid-cols-5 gap-2">
+		<Input
+			placeholder={$_('returns.entry.sender.header')}
+			bind:value={query.sender}
+			on:input={() => {
+				dispatch('filterQuery', query);
+			}}
+		/>
+		<Input
+			placeholder={$_('returns.entry.products.header')}
+			bind:value={query.product}
+			on:input={() => {
+				dispatch('filterQuery', query);
+			}}
+		/>
+	</div>
+</div>
